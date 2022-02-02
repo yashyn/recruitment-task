cd07ddecb3c7f2bcc1c62285bfe97c3ff146caee
Alexander Yashyn <a.yashyn@softteco.com>
2022-02-02 13:58:01 +0100

    added blank controller

---
+++ CarStore/CarStore/Controllers/CarController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromRoute] Guid id, CancellationToken cancellationToken)
+        {
+            return Ok();
+        }
+    }
+}
