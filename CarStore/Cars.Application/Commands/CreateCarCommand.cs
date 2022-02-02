6db2a5f923541fc70800bd12f8db71b8fd89ad64
Alexander Yashyn <a.yashyn@softteco.com>
2022-02-02 14:37:50 +0100

    added blank command and query

---
+++ CarStore/Cars.Application/Commands/CreateCarCommand.cs
@@ -0,0 +1,16 @@
+﻿using Cars.Domain.Models;
+using CarStore.Core;
+using MediatR;
+
+namespace Cars.Application.Commands
+{
+    public class CreateCarCommand : IRequest<CommandResultBase<Guid>>
+    {
+        public CreateCarCommand(Car car)
+        {
+            Car = car;
+        }
+
+        public Car Car { get; }
+    }
+}
---
+++ CarStore/Cars.Application/Commands/CreateCarCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Cars.Domain.Repos;
+using CarStore.Core;
+using MediatR;
+
+namespace Cars.Application.Commands
+{
+    public class CreateCarCommandHandler : IRequestHandler<CreateCarCommand, CommandResultBase<Guid>>
+    {
+        private readonly ICarRepo _repo;
+
+        public CreateCarCommandHandler(ICarRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<CommandResultBase<Guid>> Handle(CreateCarCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
