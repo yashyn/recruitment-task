e01f3d4e61d4d11ca612f3675af978ded4cfcc1d
Alexander Yashyn <a.yashyn@softteco.com>
2022-02-02 14:11:59 +0100

    created interface for repository

---
+++ CarStore/Cars.Domain/Repos/ICarRepo.cs
@@ -0,0 +1,14 @@
+﻿using Cars.Domain.Models;
+using System.Linq.Expressions;
+
+namespace Cars.Domain.Repos
+{
+    public interface ICarRepo
+    {
+        Task<Guid> CreateAsync(Car car, CancellationToken cancellationToken = default);
+        Task<Car> GetFirstOrDefaultAsync(Expression<Func<Car, bool>> filter, CancellationToken cancellationToken = default);
+        Task<Car[]> GetWhereAsync(Expression<Func<Car, bool>> filter, CancellationToken cancellationToken = default);
+        Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+        Task UpdateAsync(Car car, CancellationToken cancellationToken = default);
+    }
+}
