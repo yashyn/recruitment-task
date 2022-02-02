927ad534ff4d17e6cdda032a07858ec86afd0e1d
Alexander Yashyn <a.yashyn@softteco.com>
2022-02-02 14:48:47 +0100

    added create car request dto

---
+++ CarStore/CarStore/Dtos/Cars/CreateCarRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using Cars.Domain.Models;
+
+namespace CarStore.Api.Dtos.Cars
+{
+    public class CreateCarRequestDto
+    {
+        public string Vin { get; set; }
+        public string Make { get; set; }
+        public CarType Type { get; set; }
+    }
+}
