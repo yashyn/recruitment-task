e6a257ff1ffacb1abde6839c169790fbf04fdd52
Alexander Yashyn <a.yashyn@softteco.com>
2022-02-02 14:51:01 +0100

    updated migration

---
+++ CarStore/Cars.Infrastructure/Migrations/20220202135046_RemovedMake.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cars.Infrastructure.Migrations
+{
+    public partial class RemovedMake : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Make",
+                schema: "cars",
+                table: "Cars");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Make",
+                schema: "cars",
+                table: "Cars",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
