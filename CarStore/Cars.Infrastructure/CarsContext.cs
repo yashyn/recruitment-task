f2da00b04b6033832ba8b0bb1df6be512998cb1c
Alexander Yashyn <a.yashyn@softteco.com>
2022-02-02 14:17:06 +0100

    added db context for cars

---
+++ CarStore/Cars.Infrastructure/CarsContext.cs
@@ -0,0 +1,16 @@
+﻿using Cars.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cars.Infrastructure
+{
+    public class CarsContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // TODO pass from environment variable
+            optionsBuilder.UseNpgsql("Host=localhost;Username=postgres;Password=admin;Database=recrtask;SSL Mode=Disable;Port=5432");
+        }
+    }
+}
